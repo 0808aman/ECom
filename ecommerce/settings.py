344f11c3f337d5e78130bdb876f7006d8d7df7fc
Aman Pandey <amanpandey@Amans-Mac-mini.local>
2021-07-29 21:43:44 +0530

    Inital Commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -138,3 +138,7 @@ STATICFILES_FINDERS = (
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/images/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
